506d336cd9bf1ff4ba68dce7ae111ad81aea7b6b
YS739 <suk921@gmail.com>
2022-11-21 10:45:58 +0900

    main.js - add login monitoring / move handleLocation

--- js/main.js
+++ js/main.js
@@ -5,7 +5,21 @@ import { handleLocation, route } from "./router.js";
 window.addEventListener("hashchange", handleLocation);
 
 // 첫 랜딩 또는 새로고침 시 처리
-document.addEventListener("DOMContentLoaded", handleLocation);
+document.addEventListener("DOMContentLoaded", () => {
+  // 로그인 상태 모니터링
+  authService.onAuthStateChanged((user) => {
+    // Firebase 연결되면 화면 표시
+    // user === authService.currentUser 와 같은 값
+    handleLocation(); //authService가 연결 되면 handleLocation 실행
+    if (user) {
+      // 로그인 상태인 경우
+      alert("로그인 되었습니다.");
+    } else {
+      // 로그아웃 상태인 경우
+      alert("로그아웃 되었습니다.");
+    }
+  });
+});
 
 // 전역 함수 리스트
 window.route = route;
